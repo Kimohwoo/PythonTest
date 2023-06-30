c65381aa3dbe25ff040966d73b51d28c601f4466
Kimohwoo <kowkow292@naver.com>
2023-06-30 16:33:14 +0900

    0630 수업

--- TestCode.py
+++ TestCode.py
@@ -1,43 +1,25 @@
-from tkinter import *
-from tkinter import messagebox
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-def myFunc():
-    # messagebox.showinfo("우주 버튼", "크다")
-    if chk.get() == 0:
-        messagebox.showinfo("", "체크버튼이 꺼졌어요")
-    else : 
-        messagebox.showinfo("", "체크버튼이 켜졌어요")
+inFilename = 'Excel/singer.xlsx'
+outFilename = 'Excel/singer_youtube0630.xlsx'
 
-window = Tk()
-window.title("연습용")
-window.geometry("500x500")
+df_seniro = pd.read_excel(inFilename, 'senior', index_col=None)
+df_junior = pd.read_excel(inFilename, 'junior', index_col=None)
 
-chk = IntVar()
-cb1 = Checkbutton(window, text="클릭하세요", variable=chk, command=myFunc)
+df_singer = pd.concat( [df_seniro, df_junior] )
+df_singer_youtube = df_singer.sort_values(by=['유튜브 조회수'], axis=0, ascending=False)
+df_singer_youtube = df_singer_youtube.loc[:, ['아이디', '이름', '인원', '유튜브 조회수']]
 
-cb1.pack()
+x_data = df_singer_youtube['아이디']
+y_data = df_singer_youtube['유튜브 조회수']
+sizeAry = np.sqrt(df_singer_youtube['유튜브 조회수'])
+colorAry = np.random.rand(len(x_data))
+plt.scatter(x_data, y_data, s=sizeAry, c=colorAry, alpha = 0.5, cmap='Spectral')
+plt.show()
 
-# window.resizable(width=False, height=False)
-# label1 = Label(window, text="문자열1")
-# label2 = Label(window, text="문자열2", font=("궁서체", 30), fg="red")
-# label3 = Label(window, text="문자열3", bg="magenta", width=100, height=100, anchor=SE)
-# photo = PhotoImage(file="a.gif")
-# photo2 = PhotoImage(file="lex1.gif")
-# photo3 = PhotoImage(file="lex.gif")
-
-# button1 = Button(window, image=photo, command=myFunc)
-
-# label4 = Label(window, image=photo)
-# label5 = Label(window, image=photo2)
-# label6 = Label(window, image=photo3)
-
-# button1.pack()
-
-# label1.pack()
-# label2.pack()
-# label3.pack()
-# label4.pack(side=RIGHT)
-# label5.pack(side=LEFT)
-# label6.pack()
-
-window.mainloop()
+writer = pd.ExcelWriter(outFilename)
+df_singer_youtube.to_excel(writer, sheet_name='singer', index=False)
+writer.close()
+print('Save. OK~')
