b0e62e059d7949040c95ecc423ad41a21c2e67a5
Kimohwoo <kowkow292@naver.com>
2023-07-10 11:05:30 +0900

    0710 실습

--- ch9_test/Code09-04.py
+++
@@ -1,8 +0,0 @@
-import bs4
-
-webPage = open('HTML/Sample02.html', 'rt', encoding='utf-8').read()
-bsObject = bs4.BeautifulSoup(webPage, 'html.parser')
-
-tag_div = bsObject.find('div')
-print(tag_div)
-print(tag_div.text)
